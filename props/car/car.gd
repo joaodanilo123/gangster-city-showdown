142ae1467ba0b7a829742f0865cc2bef9b4d069c
joao <jddiedrich@gmail.com>
2024-07-06 12:00:01 -0300

    Feature: Adiciona Cena  do carro e asset

---
+++ props/car/car.gd
@@ -0,0 +1,11 @@
+extends Node2D
+class_name Car
+
+
+
+func _on_interaction_zone_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_interaction_zone_body_exited(body):
+	pass # Replace with function body.
