e825e95ad1dc3c68b8787cc5f640549cda488a11
João Danilo <jddiedrich@gmail.com>
2024-07-09 00:16:59 -0300

    Feature: Adiciona funcionalidade de receber dinheiro

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -2,11 +2,13 @@ extends Node
 
 var main_scene: Main
 var player: Player
+var money = 0
+
 @onready var objective_completed = false
 @onready var menu_level: Level = ResourceLoader.load("res://levels/menu/menu_level.tres", "Level")
 
 func change_level(level: Level):
 	main_scene.load_level(level)
-
+	
 func complete_level():
 	main_scene.load_level(menu_level)
--- props/projectiles/player_bullet/player_bullet.gd
+++ props/projectiles/player_bullet/player_bullet.gd
@@ -12,6 +12,6 @@ func _process(delta):
 
 func _on_area_2d_area_entered(area):
 	if(area.has_method("take_damage")):
-		area.take_damage(emitter, self)
+		area.take_damage(emitter, stats)
 	
 	queue_free()
